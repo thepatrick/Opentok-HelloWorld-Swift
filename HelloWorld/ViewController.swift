49f11fe38da55bc1177a4abf255d7cd4a8a4e3e8
Patrick Quinn-Graham <pqg@tokbox.com>
2014-06-03 23:45:54 -0700

    Add readme and make it work

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -3,23 +3,214 @@
 //  HelloWorld
 //
 //  Created by Patrick Quinn-Graham on 3/06/2014.
-//  Copyright (c) 2014 Patrick Quinn-Graham. All rights reserved.
+//  Copyright (c) 2014 TokBox, Inc. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-                            
+let videoWidth : CGFloat = 320
+let videoHeight : CGFloat = 240
+
+// *** Fill the following variables using your own Project info  ***
+// ***          https://dashboard.tokbox.com/projects            ***
+// Replace with your OpenTok API key
+let ApiKey = ""
+// Replace with your generated session ID
+let SessionID = ""
+// Replace with your generated token
+let Token = ""
+
+// Change to YES to subscribe to your own stream.
+let SubscribeToSelf = false
+
+class ViewController: UIViewController, OTSessionDelegate, OTSubscriberKitDelegate, OTPublisherDelegate {
+    
+    var session : OTSession?
+    var publisher : OTPublisher?
+    var subscriber : OTSubscriber?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Step 1: As the view is loaded initialize a new instance of OTSession
+        session = OTSession(apiKey: ApiKey, sessionId: SessionID, delegate: self)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        // Step 2: As the view comes into the foreground, begin the connection process.
+        doConnect()
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+    
+    
+    override func shouldAutorotateToInterfaceOrientation(toInterfaceOrientation: UIInterfaceOrientation) -> Bool {
+        return !(UIDevice.currentDevice().userInterfaceIdiom == .Phone &&
+            UIInterfaceOrientationIsLandscape(toInterfaceOrientation))
+    }
+    
+    // MARK: - OpenTok Methods
+
+    /**
+     * Asynchronously begins the session connect process. Some time later, we will
+     * expect a delegate method to call us back with the results of this action.
+     */
+    func doConnect() {
+        var err : OTError?
+        session?.connectWithToken(Token, error: &err)
+        if err != nil {
+            showAlert(err.description)
+            
+        }
+    }
+    
+    /**
+     * Sets up an instance of OTPublisher to use with this session. OTPubilsher
+     * binds to the device camera and microphone, and will provide A/V streams
+     * to the OpenTok session.
+     */
+    func doPublish() {
+        publisher = OTPublisher(delegate: self)
+        
+        var error : OTError?
+        session?.publish(publisher, error: &error)
+        if error != nil {
+            showAlert(error.description)
+        }
+        
+        view.addSubview(publisher?.view)
+        publisher!.view.frame = CGRect(x: 0.0, y: 0, width: videoWidth, height: videoHeight)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    /**
+     * Instantiates a subscriber for the given stream and asynchronously begins the
+     * process to begin receiving A/V content for this stream. Unlike doPublish,
+     * this method does not add the subscriber to the view hierarchy. Instead, we
+     * add the subscriber only after it has connected and begins receiving data.
+     */
+    func doSubscribe(stream : OTStream) {
+        subscriber = OTSubscriber(stream: stream, delegate: self)
+
+        var error : OTError?
+        session?.subscribe(subscriber, error: &error)
+        if error != nil {
+            showAlert(error.description)
+        }
     }
+    
+    /**
+     * Cleans the subscriber from the view hierarchy, if any.
+     */
+    func doUnsubscribe() {
+        if subscriber != nil {
+            return
+        }
+        
+        var error : OTError?
+        session?.unsubscribe(subscriber, error: &error)
+        if error != nil {
+            showAlert(error.description)
+        }
+        
+        subscriber?.view.removeFromSuperview()
+        subscriber = nil
+    }
+    
+    // MARK: - OTSession delegate callbacks
+    
+    func sessionDidConnect(session: OTSession) {
+        NSLog("sessionDidConnect (\(session.sessionId))")
 
+        // Step 2: We have successfully connected, now instantiate a publisher and
+        // begin pushing A/V streams into OpenTok.
+        doPublish()
+    }
+    
+    func sessionDidDisconnect(session : OTSession) {
+        NSLog("Session disconnected (\( session.sessionId))")
+    }
+    
+    func session(session: OTSession, streamCreated stream: OTStream) {
+        NSLog("session streamCreated (\(stream.streamId))")
 
+        // Step 3a: (if NO == subscribeToSelf): Begin subscribing to a stream we
+        // have seen on the OpenTok session.
+        if subscriber == nil && !SubscribeToSelf {
+            doSubscribe(stream)
+        }
+    }
+    
+    func session(session: OTSession, streamDestroyed stream: OTStream) {
+        NSLog("session streamCreated (\(stream.streamId))")
+        
+        if subscriber?.stream.streamId == stream.streamId {
+            doUnsubscribe()
+        }
+    }
+    
+    func session(session: OTSession, connectionCreated connection : OTConnection) {
+        NSLog("session connectionCreated (\(connection.connectionId))")
+    }
+    
+    func session(session: OTSession, connectionDestroyed connection : OTConnection) {
+        NSLog("session connectionDestroyed (\(connection.connectionId))")
+    }
+    
+    func session(session: OTSession, didFailWithError error: OTError) {
+        NSLog("session didFailWithError (%@)", error)
+    }
+    
+    // MARK: - OTSubscriber delegate callbacks
+    
+    func subscriberDidConnectToStream(subscriberKit: OTSubscriberKit) {
+        NSLog("subscriberDidConnectToStream (\(subscriberKit))")
+        if let view = subscriber?.view {
+            view.frame =  CGRect(x: 0.0, y: videoHeight, width: videoWidth, height: videoHeight)
+            self.view.addSubview(view)
+        }
+    }
+    
+    func subscriber(subscriber: OTSubscriberKit, didFailWithError error : OTError) {
+        NSLog("subscriber %@ didFailWithError %@", subscriber.stream.streamId, error)
+    }
+    
+    // MARK: - OTPublisher delegate callbacks
+    
+    func publisher(publisher: OTPublisherKit, streamCreated stream: OTStream) {
+        NSLog("publisher streamCreated %@", stream)
+
+        // Step 3b: (if YES == subscribeToSelf): Our own publisher is now visible to
+        // all participants in the OpenTok session. We will attempt to subscribe to
+        // our own stream. Expect to see a slight delay in the subscriber video and
+        // an echo of the audio coming from the device microphone.
+        if subscriber == nil && SubscribeToSelf {
+            doSubscribe(stream)
+        }
+    }
+    
+    func publisher(publisher: OTPublisherKit, streamDestroyed stream: OTStream) {
+        NSLog("publisher streamDestroyed %@", stream)
+        
+        if subscriber?.stream.streamId == stream.streamId {
+            doUnsubscribe()
+        }
+    }
+    
+    func publisher(publisher: OTPublisherKit, didFailWithError error: OTError) {
+        NSLog("publisher didFailWithError %@", error)
+    }
+    
+    // MARK: - Helpers
+
+    func showAlert(message: String) {
+        // show alertview on main UI
+        dispatch_async(dispatch_get_main_queue()) {
+            let al = UIAlertView(title: "OTError", message: message, delegate: nil, cancelButtonTitle: "OK")
+        }
+    }
+    
+    
 }
 
---
+++ README.md
@@ -0,0 +1,19 @@
+OpenTok Hello World (in swift)
+==============================
+
+To get started download the iOS SDK, and place it in `HelloWorld/Opentok.framework`, e.g. with:
+
+```bash
+$ git clone git@github.com:thepatrick/Opentok-HelloWorld-Swift.git
+$ cd Opentok-HelloWorld-Swift
+$ curl -L -O http://tokbox.com/downloads/opentok-ios-sdk-2.2
+$ tar xzvf opentok-ios-sdk-2.2 OpenTok-iOS-2.2.0-beta.3/OpenTok.framework
+$ mv OpenTok-iOS-2.2.0-beta.3/OpenTok.framework HelloWorld/OpenTok.framework
+```
+
+Then open `HelloWorld.xcodeproj` and run on your device.
+
+Warnings
+--------
+
+You must have an Xcode with support for swift. See [Introducing Swift](https://developer.apple.com/swift/) for links to download Xcode.
